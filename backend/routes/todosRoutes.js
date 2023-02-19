0c48bcda312faafdb175ba5eabc64d159bb0e0f7
unisrasheed <unisrasheed771@gmail.com>
2023-02-19 11:29:11 +0530

    updated a todosRoutes file

--- backend/routes/todosRoutes.js
+++ backend/routes/todosRoutes.js
@@ -4,7 +4,9 @@ const express =  require('express')
 const { getTodos, createTodos, updateTodos, deleteTodos } = require('../controller/todosController')
 const router =  express.Router()
 
-router.route('/').get(getTodos).post(createTodos)
+// router.route('/').get(getTodos).post(createTodos)
+router.get('/', getTodos)
+router.post('/',createTodos)
 router.route('/:id').put(updateTodos).delete(deleteTodos)
 
 
